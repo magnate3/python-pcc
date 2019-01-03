972e18365b694ab2e1dadfe859348390d3870356
watal <watal.i27e@gmail.com>
2019-01-03 18:07:35 +0900

    PCC test

--- iproute2_apply.py
+++ iproute2_apply.py
@@ -7,10 +7,10 @@ import subprocess
 def ip_route(segment_lists):
     '''add static route'''
 
-    delcmd = 'sudo ip route del ' + segment_lists['to']
+    delcmd = 'sudo ip route del ' + segment_lists['dst']
     subprocess.call(delcmd, shell=True)
     addcmd = 'sudo ip route add ' + \
-        segment_lists['to'] + ' encap mpls ' + \
-        segment_lists['segment_list'] + ' via ' + segment_lists['via']
+        segment_lists['dst'] + ' encap mpls ' + \
+        segment_lists['segment_list'] + ' via ' + segment_lists['nexthop']
     subprocess.call(addcmd, shell=True)
     return
