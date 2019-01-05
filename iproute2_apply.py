03a379b9e118b4c9b167925ae7d4d63a1ca776c1
watal <watal.i27e@gmail.com>
2019-01-05 21:39:11 +0900

    Fix bugs

--- iproute2_apply.py
+++ iproute2_apply.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 from __future__ import print_function
+import sys
 import subprocess
 
 
@@ -10,11 +11,11 @@ def ip_route(sl_info):
 
     # Can't create policy-based path
     if sl_info['segmentlist'] == 'Unreachable':
-        print('Cannot create segmentlist from {} to {}.'.format(sl_info['src'], sl_info['dst']), file=sys.stderr)
+        print('[Segment list: Error] Cannot create segmentlist from {} to {}.'.format(sl_info['src'], sl_info['dst']), file=sys.stderr)
         return -1
 
     delcmd = 'sudo ip route del ' + sl_info['dst']
-    #subprocess.call(delcmd, shell=True)
+    subprocess.call(delcmd, shell=True)
     addcmd = 'sudo ip route add ' + \
         sl_info['dst'] + ' encap mpls ' + \
         sl_info['segmentlist'] + ' via ' + sl_info['nexthop']
